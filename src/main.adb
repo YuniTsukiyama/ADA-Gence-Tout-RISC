66782d77b860b7f4eaafb97fbc32070729417eb5
Arthur Hugonnot <arthur.hugonnot@epita.fr>
2022-01-06 22:23:15 +0100

    driver: inline `Curr_Line` variable
            * src/main.adb: Here.

--- src/main.adb
+++ src/main.adb
@@ -3,14 +3,12 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with Instruction;
 with Parser;
-with Misc;
 
 procedure Main is
    Input_File  : constant String := Argument (1);
    File        : File_Type;
    Instrs      : Instruction.Instruction_List.List;
    Parser_Inst : Parser.Instance;
-   Curr_Line   : Misc.Input_Ptr;
    Curr_Instr  : Instruction.Instance;
 begin
    Open (File, In_File, Input_File);
@@ -18,8 +16,7 @@ begin
    --  Parse each instructions and build a list
    while not End_Of_File (File) loop
       --  Get the next line
-      Curr_Line := new String'(Get_Line (File));
-      Parser_Inst.Initialize (Curr_Line);
+      Parser_Inst.Initialize (new String'(Get_Line (File)));
 
       --  Parse it
       Parser_Inst.Parse (Curr_Instr);
